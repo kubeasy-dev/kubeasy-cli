322b1360d54fc3ac529f0e2b368cbe19ed0fdf96
Paul Brissaud <pbrissaud.pro@gmail.com>
2025-08-08 21:34:50 +0200

    fix: delete namespace and switch context in cleaning process
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- cmd/common.go
+++ cmd/common.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/kubeasy-dev/kubeasy-cli/pkg/api"
 	"github.com/kubeasy-dev/kubeasy-cli/pkg/argocd"
+	"github.com/kubeasy-dev/kubeasy-cli/pkg/constants"
 	"github.com/kubeasy-dev/kubeasy-cli/pkg/kube"
 )
 
@@ -35,8 +36,32 @@ func deleteChallengeResources(challengeSlug string) {
 		cancel()
 		os.Exit(1)
 	}
+
+	// Delete ArgoCD Application
 	if err := argocd.DeleteChallengeApplication(ctx, dynamicClient, challenge.Slug, argocd.ArgoCDNamespace); err != nil {
 		fmt.Fprintf(os.Stderr, "Error deleting ArgoCD Application for challenge '%s': %v\n", challengeSlug, err)
+		cancel()
+		os.Exit(1)
+	}
+
+	// Get Kubernetes clientset for namespace deletion
+	clientset, err := kube.GetKubernetesClient()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting Kubernetes clientset: %v\n", err)
+		cancel()
+		os.Exit(1)
+	}
+
+	// Delete Kubernetes namespace manually because ArgoCD does not delete namespaces even if it was created by ArgoCD
+	if err := kube.DeleteNamespace(ctx, clientset, challenge.Slug); err != nil {
+		fmt.Fprintf(os.Stderr, "Error deleting Kubernetes namespace for challenge '%s': %v\n", challengeSlug, err)
+		cancel()
+		os.Exit(1)
+	}
+
+	//switch to default namespace
+	if err := kube.SetNamespaceForContext(constants.KubeasyClusterContext, "default"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error switching to default namespace: %v\n", err)
 		cancel()
 		os.Exit(1)
 	}
